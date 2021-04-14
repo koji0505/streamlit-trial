a6a85920786e8cddcc2dcda4a6c625ce0b3a581e
ksagawa <ksagawa0505@gmail.com>
2021-04-15 01:52:58 +0900

    1st commit

---
+++ main.py
@@ -0,0 +1,17 @@
+import streamlit as st
+import numpy as np
+import pandas as pd
+
+st.title('Streamlit 超入門')
+st.write('Interactive Widgets')
+
+left_column,right_column=st.beta_columns(2)
+button=left_column.button('右カラムに文字を表示')
+
+if button:
+    right_column.write('ここは右カラム')
+
+expander1=st.beta_expander('問い合わせ1')
+expander1.write('問い合わせ1の回答')
+expander2=st.beta_expander('問い合わせ2')
+expander3=st.write('問い合わせ3の回答')
